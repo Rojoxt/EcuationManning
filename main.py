aaa29de478dd5a8bb2fddb2cc2b1d142a5e1b391
Rojoxt <delamarzp@gmail.com>
2024-06-28 12:22:22 -0500

    feat: update

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+
+
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+CORS(app)  # Esto permite solicitudes CORS desde cualquier origen
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 def manning_equation(n, A, R, S):
     return (1 / n) * A * (R ** (2/3)) * (S ** (1/2))
 
-@app.route('/calculate', methods=['POST', 'OPTIONS'])
+@app.route('/calculate', methods=['POST'])
 def calculate():
-    if request.method == 'OPTIONS':
-        response = jsonify({'message': 'Preflight request successful'})
-    else:
-        data = request.json
-        n = data.get('n')
-        A = data.get('A')
-        R = data.get('R')
-        S = data.get('S')
-        Q = manning_equation(n, A, R, S)
-        response = jsonify({'Q': Q})
-
-    response.headers.add('Access-Control-Allow-Origin', 'https://codepen.io')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-    response.headers.add('Access-Control-Allow-Methods', 'POST')
-    return response
+    data = request.json
+    n = data.get('n')
+    A = data.get('A')
+    R = data.get('R')
+    S = data.get('S')
+    Q = manning_equation(n, A, R, S)
+    return jsonify({'Q': Q})
 
 @app.route('/', methods=['GET'])
 def index():
