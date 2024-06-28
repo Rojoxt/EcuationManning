e7d2b28ffbe0d50481ada38f5282a819199d9646
Rojoxt <delamarzp@gmail.com>
2024-06-28 11:57:31 -0500

    feat: update

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Esto permite solicitudes CORS desde cualquier origen
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 def manning_equation(n, A, R, S):
     return (1 / n) * A * (R ** (2/3)) * (S ** (1/2))
 
-@app.route('/calculate', methods=['POST'])
+@app.route('/calculate', methods=['POST', 'OPTIONS'])
 def calculate():
-    data = request.json
-    n = data.get('n')
-    A = data.get('A')
-    R = data.get('R')
-    S = data.get('S')
-    Q = manning_equation(n, A, R, S)
-    return jsonify({'Q': Q})
+    if request.method == 'OPTIONS':
+        response = jsonify({'message': 'Preflight request successful'})
+    else:
+        data = request.json
+        n = data.get('n')
+        A = data.get('A')
+        R = data.get('R')
+        S = data.get('S')
+        Q = manning_equation(n, A, R, S)
+        response = jsonify({'Q': Q})
+
+    response.headers.add('Access-Control-Allow-Origin', 'https://cdpn.io')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    response.headers.add('Access-Control-Allow-Methods', 'POST')
+    return response
 
 @app.route('/', methods=['GET'])
 def index():
