df0e2ce0790f5b35dfeb1414240e136b05b0b828
Rojoxt <delamarzp@gmail.com>
2024-06-28 12:31:50 -0500

    feat: update

--- main.py
+++ main.py
@@ -7,18 +7,20 @@ app = Flask(__name__)
 CORS(app)  # Esto permite solicitudes CORS desde cualquier origen
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-def manning_equation(n, A, R, S):
-    return (1 / n) * A * (R ** (2/3)) * (S ** (1/2))
+
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
-    data = request.json
-    n = data.get('n')
-    A = data.get('A')
-    R = data.get('R')
-    S = data.get('S')
-    Q = manning_equation(n, A, R, S)
-    return jsonify({'Q': Q})
+    try:
+        data = request.json
+        n = data.get('n')
+        A = data.get('A')
+        R = data.get('R')
+        S = data.get('S')
+        result = (1 / n) * A * (R ** (2/3)) * (S ** (1/2))
+        return jsonify({'result': result}),200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 @app.route('/', methods=['GET'])
 def index():
