b5f019075071cb59c8bafddfe3b76a1d8934773f
Rojoxt <delamarzp@gmail.com>
2024-06-28 13:32:00 -0500

    feat: updatev2

--- main.py
+++ main.py
@@ -2,29 +2,29 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import math  # Importar el módulo math
 
-
 app = Flask(__name__)
 CORS(app)  # Esto permite solicitudes CORS desde cualquier origen
 CORS(app, resources={r"/*": {"origins": "*"}})
+
 
 @app.route('/calcula', methods=['POST'])
 def calcular_bernoulli():
     try:
         data = request.json
-        P = data.get('P')
-        p = data.get('p')
-        v = data.get('v')
-        g = data.get('g')
-        h = data.get('h')
-
-        resultado = P + (0.5 * p * math.pow(v, 2)) + (p * g * h)
-        return jsonify({'resultado': resultado}), 200
+        n = float(data.get('n'))  # Convertir a float si es necesario
+        A = float(data.get('A'))
+        R = float(data.get('R'))
+        S = float(data.get('S'))
+        result = (1 / n) * A * (R ** (2 / 3)) * (S ** (1 / 2))
+        return jsonify({'Q': result}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 400
+
 
 @app.route('/', methods=['GET'])
 def index():
     return "La aplicación BernoulliAPI está corriendo correctamente"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
